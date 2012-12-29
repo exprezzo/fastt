dc9c0cc5bfbdfc99a7df359297cc36507f2b6bcd
colab_01.diem <runtim3.error@gmail.com>
2012-12-29 15:09:59 -0700

    Bug en tabManager Corregido

--- js/TabManager.js
+++ js/TabManager.js
@@ -1,26 +1,25 @@
 var tab_counter = 2;
 // close icon: removes the tab on click
  
- var TabManager={
+var TabManager={
 	init:function(){
 		$tabs = $('#tabs').wijtabs({
 		   tabTemplate: '<li><a  href="#{href}">#{label}</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>'
 		});
 		this.tabs=$tabs;
-		// addTab button: opens the Add tab dialog box
-			
+		// addTab button: opens the Add tab dialog box		
 		$('#tabs span.ui-icon-close').live('click', function () {
 			var index = $('li', $tabs).index($(this).parent());
 			$tabs.wijtabs('remove', index);
-		});		
+		});
 	},
 	add:function(url,titulo,id){
 		id = id || 0;
 		var tabId = '#tabs-' + tab_counter;
 		
-		var objId = url+'?id='+id;		
-		if ( this.seleccionarTab(objId) == true) 
-			return true;		
+		var objId = url+'?id='+id;
+		if ( this.seleccionarTab(objId) == true)
+			return true;
 		tab_counter++;
 		$.ajax({
 			type: "POST",
@@ -37,29 +36,29 @@ var tab_counter = 2;
 	seleccionarTab:function(objId){
 		//¿ Que pasa con los tabs dentro de tabs ?
 		
-		var tabListaPedidos = $('div[objId="'+objId+'"]'); //role="tabPanel", 
+		var tabListaPedidos = $('#tabs > div[objId="'+objId+'"]'); //role="tabPanel", 
 		
 		if (tabListaPedidos.length == 0){
 			return false;
 		}else if (tabListaPedidos.length > 0){ //Seleccionar el tab		
-			var tabs = $('div[role="tabpanel"]');
+			var tabs = $('#tabs > div[role="tabpanel"]');
 			$.each(tabs, function(index, element) {
 				var jElement = $(element);
 				//si el elemento tiene el atributo == objId, seleccionar
 				if ( jElement.attr ){
 					if ( jElement.attr('objId')==objId ){
-						//Quita seleccion al anterior
-						var jAnt = $('div[aria-hidden="false"]');
+						//Ocultar el panel anterior
+						var jAnt = $('#tabs > div[aria-hidden="false"]');
 						jAnt.addClass('ui-tabs-hide');
 						jElement.attr('aria-hidden',true);
-						
-						var linkAnt = $('li[aria-selected="true"]');		
+						//Desmarcar el tab anterior
+						var linkAnt = $('#tabs > ul li[aria-selected="true"]');		
 						linkAnt.attr('aria-selected',false);
 						linkAnt.removeClass('ui-tabs-selected');
 						linkAnt.removeClass('ui-state-active');						 
 						
-						
-						var selector='a[href="#'+jElement.attr('id')+'"]';						
+						//Marcar el nuevo tab
+						var selector='#tabs > ul a[href="#'+jElement.attr('id')+'"]';						
 						var activeTab = $(selector).parent();
 						activeTab.attr('aria-selected',true);
 						activeTab.addClass('ui-tabs-selected');
