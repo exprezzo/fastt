f53a8f1dd2b4cf5aa6b08fea996269130dfa0475
colab_01.diem <runtim3.error@gmail.com>
2013-01-02 15:37:55 -0700

    ...

--- app/vistas/welcome.php
+++ app/vistas/welcome.php
@@ -10,14 +10,21 @@ $( function(){
 	<ul> 
 		<li>estrenando Wijmo framework (25 Dic 2012)</li>
 	</ul>
+	<hr>
 	<h3>En Proceso</h3>
 		<li><h3>Layout General</h3>
 			<ul>						
-				<li>Tab: Bug visual en firefox</li>						
+				<li>Tabs: Bug visual en firefox, la x usada para cerrar el tab, aparece en el piso</li>						
+				<li>Bug: Un tab puede abrirse dos veces: Abrir tab de acceso directo, abrir el mismo tab desde el menu principal, Erro! </li>
 			</ul>
+				
 		</li>		
-		
-	<h3>Pendientes</h3>			
-		<li>Definir lista de dispositivos y navegadores donde se haran pruebas de usabilidad</li>
-		<li>Los grids deben crecer al tamaño de pantalla disponible</li>		
+		<li><h3>Catalogo Pedidos</h3></li>
+		<li><h4>Lista de Pedidos</h4></li>		
+		<li><h4>Edicion de Pedido</h4></li>
+	<hr>
+	<h3>Otros</h3>			
+		<li>Definir lista de dispositivos y navegadores donde debe funcionar el sistema</li>
+		<li>Definir las acciones que debe tener el toolbar de edicion y el de busqueda</li>
+		<li>Definir Tema principal para adaptar la pantalla de login</li>
 </div>
